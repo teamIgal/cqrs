6a8d86438d27b2f441c1bddf9a685bdef320a991
12411J <12411J>
2019-09-16 14:48:05 +0300

    add typing to command return value

--- src/interfaces/commands/command-handler.interface.ts
+++ src/interfaces/commands/command-handler.interface.ts
@@ -1,5 +1,5 @@
 import { ICommand } from './command.interface';
 
-export interface ICommandHandler<T extends ICommand = any> {
-  execute(command: T): Promise<any>;
+export interface ICommandHandler<T extends ICommand = any, TRes = any> {
+  execute(command: T): Promise<TRes>;
 }
